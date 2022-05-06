a096f1e0b27901f4c3124f0cde2dfaec54281717
newcoderwwg <102198537+newcoderwwg@users.noreply.github.com>
2022-05-05 20:01:48 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+var first_name="Nathaniel";
+console.log(first_name);
+var last_name=" Carpenter";
+console.log(last_name);
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+var length_of_name=full_name.length;
+console.log(length_of_name);
+var change_name="One small sentance";
+console.log(change_name);
+var replace_sentence=change_name.replace("small","big");
+console.log(replace_sentence);
+var rav="ALL CAPS";
+console.log(rav);
+var arv=rav.toLowerCase();
+console.log(arv);
+var upper="no caps";
+console.log(upper);
+var lower=upper.toUpperCase();
+console.log(lower);
+
+
+
+
+var One= 1;
+console.log(One);
+var two= 9;
+console.log(two);
+var subtract= One-two;
+console.log(subtract);
+
+var add= One+two;
+console.log(add);
+var multiply= One*two;
+console.log(multiply);
+var divide= One/two;
+console.log(divide);
